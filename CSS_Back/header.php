38fc1841d437fd12029f539bff4a1a5d6625238a
AHNEUNHEE <4off4@naver.com>
2022-07-07 13:12:54 +0900

    commit1

---
+++ CSS_Back/header.php
@@ -0,0 +1,118 @@
+<div id="header">
+<!-- 모바일메뉴 버튼 -->
+    <button id="gnbTrigger">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+    </button>
+    <!-- // 모바일메뉴 버튼 -->
+    <!-- gnb wrap -->
+    <div id="gnb_wrap">
+     <h1 id="logo">
+            <a href="{{$template}}"><img src="{{$template}}/img/logo.png" class="pc" alt="Logo" /><img src="{{$template}}/img/logom.png" class="mobile" alt="Logo" /></a>
+        </h1>
+        <a class="gnb_top" href="#;"><i class="xi-profile-o"></i> LOGIN</a>
+        <div class="mobile-scroll">
+            <div class="mobile-scroll-y">
+                <ul id="gnb">
+                    <li class="dep1">
+                        <a href="{{$template}}/about/hospital/sub1_1.php?top=1&sub=1" target="_self" >da Re-Fit</a>
+                        <ul class="submnu">
+                            <li><a href="{{$template}}/about/hospital/sub1_1.php?top=1&sub=1" target="_self"><span>병원소개</span></a></li>
+                                <li><a href="{{$template}}/about/doctor/sub2_1.php?top=1&sub=2" target="_self"><span>의료진소개</span></a></li>
+                                <li><a href="{{$template}}/about/sub3.php?top=1&sub=3" target="_self"><span>장비소개</span></a></li>
+                                <li><a href="{{$template}}/about/sub5.php?top=1&sub=5" target="_self"><span>오시는길</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="dep1">
+                        <a href="{{$template}}/menu2/sub1/sub1_1.php?top=2&sub=1" target="_self" >Body MBTI</a>
+                        <ul class="submnu">
+                            <li><a href="{{$template}}/menu2/sub1/sub1_1.php?top=2&sub=1" target="_self"><span>Body MBTI 뜻</span></a></li>
+                                <li><a href="{{$template}}/menu2/sub2/sub2_1.php?top=2&sub=2" target="_self"><span>M타입</span></a></li>
+                                <li><a href="{{$template}}/menu2/sub3/sub3_1.php?top=2&sub=3" target="_self"><span>S타입</span></a></li>
+                                <li><a href="{{$template}}/menu2/sub4/sub4_1/sub4_1_1.php?top=2&sub=4" target="_self"><span>F타입</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="dep1">
+                        <a href="{{$template}}/menu3/sub1/sub1_1.php?top=3&sub=1" target="_self" >Lifting</a>
+                       <!-- <ul class="submnu">
+                            <li><a href="{{$template}}/menu3/sub1/sub1_1.php?top=3&sub=1" target="_self"><span>서브메뉴1</span></a></li>
+                                <li><a href="{{$template}}/menu3/sub2.php?top=3&sub=2" target="_self"><span>서브메뉴2</span></a></li>
+                                <li><a href="{{$template}}/menu3/sub3.php?top=3&sub=3" target="_self"><span>서브메뉴3</span></a></li>
+                        </ul>-->
+                    </li>
+                    <li class="dep1">
+                        <a href="{{$template}}/menu4/sub1.php?top=4&sub=1" target="_self" >Tightening</a>
+                       <!-- <ul class="submnu">
+                            <li><a href="{{$template}}/menu4/sub1.php?top=4&sub=1" target="_self"><span>서브메뉴1</span></a></li>
+                                <li><a href="{{$template}}/menu4/sub2.php?top=4&sub=2" target="_self"><span>서브메뉴2</span></a></li>
+                        </ul>-->
+                    </li>
+                    <li class="dep1">
+                        <a href="{{$template}}/menu5/sub1.php?top=5&sub=1" target="_self" >Cutting</a>
+                        <!--<ul class="submnu">
+                            <li><a href="{{$template}}/menu5/sub1.php?top=5&sub=1" target="_self"><span>서브메뉴1</span></a></li>
+                                <li><a href="{{$template}}/menu5/sub2.php?top=5&sub=2" target="_self"><span>서브메뉴2</span></a></li>
+                        </ul>-->
+                    </li>
+                    <li class="dep1">
+                        <a href="{{$template}}/menu6/sub1.php?top=6&sub=1" target="_self" >Shaping</a>
+                        <!--<ul class="submnu">
+                            <li><a href="{{$template}}/menu6/sub1.php?top=6&sub=1" target="_self"><span>서브메뉴1</span></a></li>
+      </ul>-->
+
+                    <li class="dep1">
+                        <a href="{{$template}}/reservation/sub1.php?top=8&sub=1" target="_self" >Circulating </a>
+                        <!--<ul class="submnu">
+                            <li><a href="{{$template}}/reservation/sub1.php?top=8&sub=1" target="_self"><span>웨딩</span></a></li>
+                            <li><a href="{{$template}}/reservation/sub2.php?top=8&sub=2" target="_self"><span>임산부산전후</span></a></li>
+                         <li><a href="{{$template}}/reservation/sub3.php?top=8&sub=3" target="_self"><span>온라인예약</span></a></li>
+                             <li><a href="{{$template}}/reservation/sub4.php?top=8&sub=4" target="_self"><span>예약확인</span></a></li>
+                        </ul> -->
+                    </li>
+
+
+<li class="dep1">
+                        <a href="{{$template}}/reservation/sub1.php?top=8&sub=1" target="_self" >Tailored Fit </a>
+                        <!--<ul class="submnu">
+                            <li><a href="{{$template}}/reservation/sub1.php?top=8&sub=1" target="_self"><span>테일러드핏 뜻</span></a></li>
+                            <li><a href="{{$template}}/reservation/sub2.php?top=8&sub=2" target="_self"><span>프로그램설명</span></a></li>
+                             <li><a href="{{$template}}/reservation/sub3.php?top=8&sub=3" target="_self"><span>웨딩</span></a></li>
+                             <li><a href="{{$template}}/reservation/sub4.php?top=8&sub=4" target="_self"><span>부종</span></a></li>
+                        </ul> -->
+                    </li>
+
+
+
+
+                    <li class="dep1">
+                        <a href="{{$template}}/community/sub1.php?top=9&sub=1" target="_self" >Notice</a>
+                        <ul class="submnu">
+                            <li><a href="{{$template}}/community/sub2.php?top=9&sub=2" target="_self"><span>치료후기</span></a></li>
+                            <li><a href="{{$template}}/community/sub3.php?top=9&sub=3" target="_self"><span>언론보도</span></a></li>
+                            <li><a href="{{$template}}/community/sub4.php?top=9&sub=4" target="_self"><span>프로모션</span></a></li>
+                            <li><a href="{{$template}}/community/sub1.php?top=9&sub=1" target="_self"><span>공지사항</span></a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+  </div>
+        <!-- // gnb -->
+        <ul class="sns_link">
+   {{if $use_member == 'Y'}}
+             {{if $member_id}}
+            <li><a href="{{$template}}/member/edit.php"><i class="xi-user-o"></i></a></li>
+            <li><a href="{{$template}}/member/login.php?com_member_basic=logout"><i class="xi-lock"></i></a></li>
+            {{else}}
+            <li><a href="{{$template}}/member/join.php"><i class="xi-unlock"></i></a></li>
+            <li><a href="{{$template}}/member/login.php"><i class="xi-pen-o"></i></a></li>
+            {{/if}}
+            {{/if}}
+        </ul>
+    </div> 
+    <!-- // gnb wrap -->
+ 
+    
+</div>
+</div>
